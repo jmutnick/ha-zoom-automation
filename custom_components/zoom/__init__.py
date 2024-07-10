d973d8288e935c471e411461f4516018ecb32e1c
Jonathan Mutnick <jmutnick@users.noreply.github.com>
2024-07-10 13:49:39 -0400

    Update __init__.py
    Fixed async issue

--- custom_components/zoom/__init__.py
+++ custom_components/zoom/__init__.py
@@ -143,11 +143,11 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     # Register view
     hass.http.register_view(ZoomWebhookRequestView())
 
-    for platform in PLATFORMS:
-        hass.async_create_task(
-            hass.config_entries.async_forward_entry_setup(entry, platform)  
-        )
-
+    #for platform in PLATFORMS:
+    #    hass.async_create_task(
+    #        hass.config_entries.async_forward_entry_setup(entry, platform)  
+    #    )
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
 
